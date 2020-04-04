4bb40a1175b015a89012640383a9fe8162c4b69a
shun-github <shunsuke.suzuki@accenture.com>
2020-04-04 16:47:26 +0900

    20200404

--- www/main/main.js
+++ www/main/main.js
@@ -3,6 +3,18 @@ var UserID = localStorage.getItem('UserID');
 if (UserID === null){$(function(){$.magnificPopup.open({items:{src:'<div class="inline-popup">有難うございます</div>'}, type:'inline'});})} else{alert('Boy');}};
 
 
+var body = document.getElementsByTagName('body')[0];
+ 
+function linkUrl() {
+    location.href = 'entry/first.html'
+};
+ 
+var bt3 = document.getElementById('button3');
+bt3.addEventListener('click', function(){
+    body.classList.add('anime3');
+    setTimeout(linkUrl, 1500);
+});
+
 
 
 
